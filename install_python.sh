c5c75320c2d67d679519532e4c22d3fd00c2a73f
Ruben Paz <me@ruben.io>
2019-10-12 07:18:28 +0100

    Install update python packages

--- install_python.sh
+++ install_python.sh
@@ -4,10 +4,8 @@ set -e
 
 sudo apt install -y \
     virtualenv \
-    python-pip \
-    default-libmysqlclient-dev \
-    build-essential \
+    python3-pip \
     python3-dev \
     python3-venv
 
-pip install --upgrade setuptools wheel
+pip3 install --upgrade setuptools wheel
