8fd7fc00f7770aab186c083011b7a3b08f3d15dd
Ruben Paz <me@ruben.io>
2023-01-30 20:40:03 +0000

    ESC from selection

--- nvim/configs/keys.lua
+++ nvim/configs/keys.lua
@@ -2,10 +2,11 @@
 
 local function nmap(k, cmd) vim.api.nvim_set_keymap('n', k, cmd, { noremap = true }) end
 
-nmap('<c-n>', ':NERDTreeToggle<CR>');
+nmap('<c-n>', '<cmd>NERDTreeToggle<CR>');
 
 -- Telescope
 nmap('<leader>ff', '<cmd>Telescope find_files<cr>');
 nmap('<leader>fg', '<cmd>Telescope live_grep<cr>');
 nmap('<leader>fb', '<cmd>Telescope buffers<cr>');
 nmap('<leader>fh', '<cmd>Telescope help_tags<cr>');
+nmap('<ESC>', '<cmd>noh<cr>');
