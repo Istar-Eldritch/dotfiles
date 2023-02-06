919fca6ea7743b0e21d5427128da1695d8a1bda2
Ruben Paz <me@ruben.io>
2023-02-06 09:50:57 +0000

    Use lsp with telescope

--- nvim/configs/lsp.lua
+++ nvim/configs/lsp.lua
@@ -2,27 +2,43 @@ local nvim_lsp = require('lspconfig')
 
 local pid = vim.fn.getpid()
 
+-- Mapping opts
+local opts = { noremap=true, silent=true }
+
 local on_attach = function(_, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
+  -- Navigation
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<leader>fh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<leader>fH', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+
+  -- Telescope bindings
+  buf_set_keymap('n', '<leader>fsw', '<cmd>Telescope lsp_workspace_symbols<CR>', opts)
+  buf_set_keymap('n', '<leader>fsW', '<cmd>Telescope lsp_dynamic_workspace_symbols<CR>', opts)
+  buf_set_keymap('n', '<leader>fsd', '<cmd>Telescope lsp_document_symbols<CR>', opts)
+  buf_set_keymap('n', '<leader>fr', '<cmd>Telescope lsp_references<CR>', opts)
+  buf_set_keymap('n', '<leader>fd', '<cmd>Telescope lsp_definitions<CR>', opts)
+
+  buf_set_keymap('n', '<leader>fi', '<cmd>Telescope lsp_implementations<CR>', opts)
+  buf_set_keymap('n', '<leader>ft', '<cmd>Telescope lsp_type_definitions<CR>', opts)
+  buf_set_keymap('n', '<leader>fci', '<cmd>Telescope lsp_incoming_calls<CR>', opts)
+  buf_set_keymap('n', '<leader>fco', '<cmd>Telescope lsp_outgoin_calls<CR>', opts)
+  buf_set_keymap('n', '<leader>fD', '<cmd>lua require(\'telescope.builtin\').diagnostics()<CR>', opts)
+
+  -- Diagnostics
+  buf_set_keymap('n', '<leader>dd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap('n', '<leader>dk', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', '<leader>dj', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<leader>dq', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
   buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+  buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+
 end
 
 nvim_lsp.pyright.setup { on_attach = on_attach }
@@ -30,7 +46,11 @@ nvim_lsp.rust_analyzer.setup { on_attach = on_attach }
 nvim_lsp.tsserver.setup { on_attach = on_attach }
 nvim_lsp.sumneko_lua.setup { on_attach = on_attach }
 nvim_lsp.omnisharp.setup {
-    on_attach = on_attach,
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr);
+      -- cs requires this patch to work with assembly files
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>fd', '<cmd>lua require(\'omnisharp_extended\').telescope_lsp_definitions()<CR>', opts)
+    end,
     handlers = {
       ["textDocument/definition"] = require('omnisharp_extended').handler,
     },
