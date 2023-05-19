148e4d942dd3ef971f223c7c61b92696a35a0b88
Ruben Paz <me@ruben.io>
2023-05-19 17:17:36 +0100

    Support spaces

--- utils/encrypt_dir.lua
+++ utils/encrypt_dir.lua
@@ -1,22 +1,57 @@
-#!/usr/local/bin/lua
+#!lua
+
+
+---@param t any
+---@return string
+local function serialize(t)
+  local typ = type(t)
+  if (typ == "table") then
+    local str = "{\n"
+    local len = #t
+    local i = 0
+    for key, value in pairs(t) do
+      i = i + 1
+      str = str .. serialize(key) .. " => " .. serialize(value)
+      if (i < len) then
+        str = str .. ",\n"
+      end
+    end
+    return str .. "\n}\n"
+  else
+    return tostring(t)
+  end
+end
 
 
 ---@param stat string
 ---@return string[]
 local function parse_file_stat(stat)
   local bits = {}
-  for w in string.gmatch(stat, "[%w%:%._-]+") do
-    table.insert(bits, w)
+  local bit_i = 1
+  local len = stat:len()
+  for i = 1, len, 1 do
+    local ch = stat:sub(i, i)
+    local last = bits[bit_i]
+    if (last == nil) then
+      last = ""
+    end
+    if (bit_i == 10 or ch ~= " ") then
+      bits[bit_i] = last .. ch
+    elseif (ch == " " and last ~= "") then
+      bit_i = bit_i + 1;
+      table[bit_i] = ""
+    end
   end
   return bits
 end
 
 
 local function scandir(path, f)
-  local handle = io.popen("ls -lsa " .. path)
+  local handle = io.popen("ls -ls " .. path)
   if (handle) then
-    for filename in handle:lines() do
-      local stat = parse_file_stat(filename)
+    handle:read()
+    for line in handle:lines() do
+      local stat = parse_file_stat(line)
       local filetype = stat[2]:sub(1, 1)
       local filename = stat[10]
       if (filename and filename:find(".gpg") == nil and filename:sub(1, 1) ~= ".") then
@@ -32,27 +67,6 @@ end
 
 local function encrypt(path)
   return os.execute("gpg --encrypt --recipient me@ruben.io " .. path)
-end
-
----@param t any
----@return string
-local function serialize(t)
-  local typ = type(t)
-  if (typ == "table") then
-    local str = "{ "
-    local len = #t
-    local i = 0
-    for key, value in pairs(t) do
-      i = i + 1
-      str = str .. serialize(key) .. " => " .. serialize(value)
-      if (i < len) then
-        str = str .. ",\n"
-      end
-    end
-    return str .. " }\n"
-  else
-    return tostring(t)
-  end
 end
 
 ---@param path string
